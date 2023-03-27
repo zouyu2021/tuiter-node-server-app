f5583ddab55c13d2c18d392657e016600286f54f
zouyu2021 <98063638+zouyu2021@users.noreply.github.com>
2023-03-27 19:36:04 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import HelloController from "./controllers/hello-controller.js";
+import UserController from "./controllers/users/users-controller.js";
+import TuitsController from "./controllers/tuits/tuits-controller.js";
+import cors from 'cors';
+
+const app = express()
+app.use(cors());
+app.use(express.json());
+TuitsController(app)  // register the tuits controller
+HelloController(app)    // register the hello controller
+UserController(app)     // register the user controller
+app.listen(process.env.PORT || 4000);
